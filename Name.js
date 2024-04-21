fc70f5bdd05b91c007ac113f7cbef3046d36f29f
Naveed Ahmed Farooqui <162836180+naveed303708@users.noreply.github.com>
2024-04-21 23:40:59 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Naveed', 'Ahmed', 'Farooqi'];
+const message = 'welcome to typescript class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Naveed','Ahmed','Farooqi'] ;
+
+const message :string = 'welcome to typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]);
+
+} 
